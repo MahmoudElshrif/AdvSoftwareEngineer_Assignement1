22d2c9c39777186615c158b85b596f96ce9c4565
Mostafa205dev <seso63412@gmail.com>
2025-11-12 00:43:08 +0200

    done enum

---
+++ Enumeration/OrderStatus.java
@@ -0,0 +1,8 @@
+package Enumeration;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    CANCELLED
+}
---
+++ Enumeration/OrderType.java
@@ -0,0 +1,7 @@
+package Enumeration;
+
+public enum OrderType {
+    DINE_IN,
+    TAKEAWAY,
+    DELIVERY
+}
